ef248598639e2ee0402c31bfb4fb0e50ba043a96
Brian Silah <unpervertedkid@gmail.com>
2023-03-08 19:32:57 +0300

    Entity class to represent a project

---
+++ src/main/java/com/silah/projecthub/entities/Project.java
@@ -0,0 +1,53 @@
+package com.silah.projecthub.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String description;
+    private Category category;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public boolean isValid() {
+        return name != null && !name.isEmpty() && description != null && !description.isEmpty() && category != null;
+    }
+}
